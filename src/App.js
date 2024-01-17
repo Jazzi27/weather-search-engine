aea88591b90150426dbd8d5606c470309f0f03de
Jazzi27 <jazzi.garrido@gmail.com>
2024-01-17 09:47:43 -0500

    Added Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jasmyn Garrido {""}
+        <a
+          href="https://github.com/Jazzi27/weather-search-engine"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
