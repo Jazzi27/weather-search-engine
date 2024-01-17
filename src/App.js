5f3341333f4974112220ce33aac0bec5425f628d
Jazzi27 <jazzi.garrido@gmail.com>
2024-01-17 10:01:37 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jasmyn Garrido {""}
-        <a
-          href="https://github.com/Jazzi27/weather-search-engine"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div classNmae="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jasmyn Garrido and is {""}
+          <a
+            href="https://github.com/Jazzi27/weather-search-engine"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
